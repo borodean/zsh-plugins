c588f51260648224af11c59a3499b3e7398d9e19
Vadym Borodin <borodean@gmail.com>
2024-05-29 17:40:51 +0300

    Add the "nvm" plugin

---
+++ nvm/nvm.plugin.zsh
@@ -0,0 +1,8 @@
+# Set up environment
+export NVM_DIR=$HOME/.nvm
+
+# Load nvm
+source $(brew --prefix)/opt/nvm/nvm.sh
+
+# Load autocompletions
+source $(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm
